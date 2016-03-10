b12292684ad06c5664d6037ba9f328eaaf1cc936
Tasya Aditya Rukmana <tadityar@Tasyas-MacBook-Pro.local>
2016-03-10 16:15:50 +0800

    Added contact forms as partials

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -8,8 +8,16 @@ class PortfolioController < ApplicationController
     @project = Project.find(params[:id])
   end
 
+  def create
+  end
+
   #This will be the access to your contact me page, might be used later as a partial at the bottom of the landing page
   def contact
-
+    @contact = Message.new
+    @contact.title = params[:title]
+    @contact.email = params[:email]
+    @contact.content = params[:content]
+    @contact.save
+    redirect_to root_path
   end
 end
