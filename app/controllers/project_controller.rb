3fcd47176e5f9b5e6611ecbd6ed2ed174e6f5574
yaojie <limyaojie93@gmail.com>
2016-03-10 22:50:55 +0800

    move project show to projectcontroller with partial

---
+++ app/controllers/project_controller.rb
@@ -0,0 +1,16 @@
+class ProjectController < ApplicationController
+  #This will be the controller for your landing page
+  def show
+    @project = Project.find(params[:id])
+  end
+  
+  #This will be the access to your contact me page, might be used later as a partial at the bottom of the landing page
+  def contact
+    @contact = Message.new
+    @contact.title = params[:title]
+    @contact.email = params[:email]
+    @contact.content = params[:content]
+    @contact.save
+    redirect_to root_path
+  end
+end
