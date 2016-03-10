4a9bd3214e007db6878485054bc19b2830b050e5
yaojie <limyaojie93@gmail.com>
2016-03-10 22:56:41 +0800

    move contact to separate controller

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -4,13 +4,4 @@ class ProjectController < ApplicationController
     @project = Project.find(params[:id])
   end
   
-  #This will be the access to your contact me page, might be used later as a partial at the bottom of the landing page
-  def contact
-    @contact = Message.new
-    @contact.title = params[:title]
-    @contact.email = params[:email]
-    @contact.content = params[:content]
-    @contact.save
-    redirect_to root_path
-  end
 end
